cf74adfe1d9863e4d00b2640e399961ee8a1b2f8
Todd Li <yan.li@catalina.com>
2018-06-08 19:13:59 +0900

    add placeholder files

---
+++ LYCNews/Classes/General/Core/CorePlaceHolder.swift
@@ -0,0 +1,21 @@
+//
+//  CorePlaceHolder.swift
+//  LYCNews
+//
+//  Created by test on 2018/06/08.
+//  Copyright © 2018 iStack. All rights reserved.
+//
+
+import UIKit
+
+class CorePlaceHolder: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
