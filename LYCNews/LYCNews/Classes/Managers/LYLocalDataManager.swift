883d6b21c861bbf5f2c40c3e6b3135eb1144a32b
Todd Li <liyan2060@gmail.com>
2018-06-08 10:44:45 +0900

    add some init files

---
+++ LYCNews/LYCNews/Classes/Managers/LYLocalDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  LYLocalDataManager.swift
+//  LYCNews
+//
+//  Created by LiYan on 2018/6/8.
+//  Copyright © 2018年 iStack. All rights reserved.
+//
+
+import UIKit
+
+class LYLocalDataManager: NSObject {
+
+}
